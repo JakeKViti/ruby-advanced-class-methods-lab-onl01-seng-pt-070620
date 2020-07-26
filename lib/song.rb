3b4798a3018f40d90d7d08a843879d40e95afc3e
Jacob Viti <thevitij64@gmail.com>
2020-07-26 00:32:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,7 @@ class Song
   end
 
   def Song.find_by_name(name)
-   #self.all.find{|song| song.name = name}
-   self.all.detect{|song| song.name = name}
+   @@all.find{|song| song.name = name}
   end
 
   def Song.destroy_all
